80e2edcc79e1b3b34e4a1aaaeec68dc16571dfac
senaru <s3696653@student.rmit.edu.au>
2020-07-05 15:04:08 +1000

    Adding Keys

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ function Lake({ name }) {
 
 const lakeList = [
     { id: "1", name: "Echo", trailhead: "Echo" },
-    { id: "1", name: "Maud", trailhead: "Wrights" },
-    { id: "1", name: "Cascade", trailhead: "Bayview" }
+    { id: "2", name: "Maud", trailhead: "Wrights" },
+    { id: "3", name: "Cascade", trailhead: "Bayview" }
 ];
 
 function App({ lakes }) {
     return (
         <div>
             {lakes.map(lake => (
-                <div>
+                <div key={lake.id}>
                     <h2>{lake.name}</h2>
                     <p>Accessed by {lake.trailhead}</p>
                 </div>
