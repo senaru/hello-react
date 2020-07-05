662d2252a8cebf03054615f73aabc932e2f15c69
senaru <s3696653@student.rmit.edu.au>
2020-07-05 15:16:33 +1000

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,29 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Lake({ name }) {
-    return <h1>{name}</h1>
-}
-
-const lakeList = [
-    { id: "1", name: "Echo", trailhead: "Echo" },
-    { id: "2", name: "Maud", trailhead: "Wrights" },
-    { id: "3", name: "Cascade", trailhead: "Bayview" }
-];
-
-function App({ lakes }) {
     return (
         <div>
-            {lakes.map(lake => (
-                <div key={lake.id}>
-                    <h2>{lake.name}</h2>
-                    <p>Accessed by {lake.trailhead}</p>
-                </div>
-            ))}
-        </div>
-    );
+            <h1>Visit {name}</h1>
+        </div>);
+}
+
+function SkiResort({ name }) {
+    return (
+        <div>
+            <h1>Visit {name}!</h1>
+        </div>);
+}
+
+function App(props) {
+    if (props.season == "summer") {
+        return <Lake name="Jenny Lake" />;
+    } else if (props.season == "winter") {
+        return <SkiResort name="Jackson Hole Mountain Resort" />;
+    }
 }
 
 ReactDOM.render(
-    <App lakes={lakeList} />,
+    <App season="summer" />,
     document.getElementById('root')
 );
