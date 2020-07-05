04aefcccfaa3f9fdf57824887d5b5135a4c12343
senaru <s3696653@student.rmit.edu.au>
2020-07-05 13:57:03 +1000

    React Properties

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(){
+function Hello(props){
+    console.log(props);
     return(
-    <div>
-        <h1>Welcome to React</h1>
-        <p>Let's build something cool.</p>
-    </div>
+        <div>
+            <h1>Welcome to {props.library}</h1>
+            <p>Let's build something cool.</p>
+            <p>{props.message}</p>
+            <p>{Object.keys(props).length} Props Total</p>
+        </div>
     );
 }
 
 ReactDOM.render(
-  <Hello/>,
+  <Hello library="React" message="Have Fun!" num={3}/>,
   document.getElementById('root')
 );
