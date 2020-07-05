35c93b9e3352e9d01d8fb11eb68f5908d8f9fae3
senaru <s3696653@student.rmit.edu.au>
2020-07-05 13:46:12 +1000

    React Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Hello(){
+    return(
+    <div>
+        <h1>Welcome to React</h1>
+        <p>Let's build something cool.</p>
+    </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello/>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
