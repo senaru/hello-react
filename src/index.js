3befd2a4597bd51c8dc93a6d0b3d0f2a92e33936
senaru <s3696653@student.rmit.edu.au>
2020-07-05 21:23:28 +1000

    useEffects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function CheckBox() {
     const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        alert(`checked: ${checked.toString()}`);
+    });
+
     return (
         <>
             <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
