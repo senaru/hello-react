e274acce09f5db884666517cfc7314e74eac70c1
senaru <s3696653@student.rmit.edu.au>
2020-07-05 21:32:45 +1000

    Updating useEffects with dependency arrays

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function CheckBox() {
-    const [checked, setChecked] = useState(false);
+function App() {
+    const [val, setVal] = useState("");
+    const [val2, setVal2] = useState("");
 
     useEffect(() => {
-        alert(`checked: ${checked.toString()}`);
-    });
+        console.log(`field 1: ${val}`);
+    }, [val]);
+
+    useEffect(() => {
+        console.log(`field 2: ${val2}`);
+    }, [val2]);
 
     return (
         <>
-            <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
-            {checked ? "checked" : "not checked"}
+            <label>
+                Favorite phrase:
+                <input value={val} onChange={e => setVal(e.target.value)} />
+            </label>
+            <br />
+            <label>
+                Second Favorite phrase:
+            <input value={val2} onChange={e => setVal2(e.target.value)} />
+            </label>
         </>
     )
 }
 
 ReactDOM.render(
-    <CheckBox />,
+    <App />,
     document.getElementById('root')
 );
 
