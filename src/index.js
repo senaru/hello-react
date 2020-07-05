eb5e05e59fb0937687272718784d27ee138f5d9d
senaru <s3696653@student.rmit.edu.au>
2020-07-05 14:55:41 +1000

    Rendering lists of objects

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake({name}){
+function Lake({ name }) {
     return <h1>{name}</h1>
 }
 
 const lakeList = [
-    "Echo Lake",
-    "Maud Lake",
-    "Cascade Lake"
+    { id: "1", name: "Echo", trailhead: "Echo" },
+    { id: "1", name: "Maud", trailhead: "Wrights" },
+    { id: "1", name: "Cascade", trailhead: "Bayview" }
 ];
 
-function App({lakes}){
-    return(
-        <ul>
-            {lakes.map(lake =>(
-                <li>{lake}</li>
+function App({ lakes }) {
+    return (
+        <div>
+            {lakes.map(lake => (
+                <div>
+                    <h2>{lake.name}</h2>
+                    <p>Accessed by {lake.trailhead}</p>
+                </div>
             ))}
-        </ul>
+        </div>
     );
 }
 
 ReactDOM.render(
-  <App lakes = {lakeList}/>,
-  document.getElementById('root')
+    <App lakes={lakeList} />,
+    document.getElementById('root')
 );
